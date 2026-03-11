2e2035137451887654019adb5a11440823b3fdbf
Twocheslch <iracches@gmail.com>
2026-03-11 15:30:36 -0600

    test names

---
+++ week3/test_names.py
@@ -0,0 +1,11 @@
+from names import make_full_name, \
+    extract_family_name, extract_given_name
+import pytest
+
+
+def test_make_full_name():
+    assert test_make_full_name
+
+
+
+pytest.main(["-v", "--tb=line", "-rN",__file__])
