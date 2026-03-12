b6d5c6f3019d85a847b8883ef02ea78b3988a682
Twocheslch <iracches@gmail.com>
2026-03-12 15:02:40 -0600

    fytd

---
+++ week3/test_water_flow.py
@@ -0,0 +1,13 @@
+from pytest import approx
+import pytest
+
+def test_water_column_height():
+    test1 = [0, 0, 0]
+    test2 = [0, 10, 7.5]
+    test3 = [25, 0, 25]
+    test4 = [48.3, 12.8, 57.9]
+    
+    
+    
+    
+    assert
---
+++ week3/water_flow.py
@@ -0,0 +1,3 @@
+def water_column_height(tower_height, tank_height):
+    water_column_height = tower_height + (3 * tank_height) / 4
+    return water_column_height
