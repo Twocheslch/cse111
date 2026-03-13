0d3ba4b5f831df0d9f47c6a97e1dde8a41d7a2c7
Twocheslch <iracches@gmail.com>
2026-03-13 13:33:33 -0600

    spacing

--- week3/water_flow.py
+++ week3/water_flow.py
@@ -15,31 +15,24 @@ KPA_PER_PSI = 6.894757293168361
 def water_column_height(tower_height, tank_height):
     return tower_height + (3 * tank_height / 4)
 
-
 def pressure_gain_from_water_height(height):
     return (WATER_DENSITY * EARTH_GRAVITY * height) / 1000
-
 
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
     return (-friction_factor * pipe_length * WATER_DENSITY * fluid_velocity ** 2) / (2000 * pipe_diameter)
 
-
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
     return (-0.04 * WATER_DENSITY * fluid_velocity ** 2 * quantity_fittings) / 2000
 
-
 def reynolds_number(hydraulic_diameter, fluid_velocity):
     return (WATER_DENSITY * hydraulic_diameter * fluid_velocity) / WATER_VISCOSITY
-
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number_value, smaller_diameter):
     k = (0.1 + 50 / reynolds_number_value) * ((larger_diameter / smaller_diameter) ** 4 - 1)
     return (-k * WATER_DENSITY * fluid_velocity ** 2) / 2000
 
-
 def kpa_to_psi(kpa):
     return kpa / KPA_PER_PSI
-
 
 def main():
     tower_height = float(input("Height of water tower (meters): "))
